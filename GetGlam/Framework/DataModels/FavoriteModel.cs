5bcf70c9bb30bed25d3435533ab885e42c228f43
Chris <csgephardt@gmail.com>
2019-12-29 01:36:46 -0500

    Initialize the favorite model to default values

--- GetGlam/Framework/DataModels/FavoriteModel.cs
+++ GetGlam/Framework/DataModels/FavoriteModel.cs
@@ -1,37 +1,43 @@
-﻿
+﻿using Microsoft.Xna.Framework;
+
 namespace GetGlam.Framework.DataModels
 {
-    /// <summary>Class the allows the player to save their customization layout</summary>
     public class FavoriteModel
     {
+        //Wether the favorite is default one
+        public bool IsDefault = true;
+
         //Whether the player is male
-        public bool IsMale;
+        public bool IsMale = true;
 
         //The base index
-        public int BaseIndex;
+        public int BaseIndex = 0;
 
         //The skin index
-        public int SkinIndex;
+        public int SkinIndex = 0;
 
         //The hair index
-        public int HairIndex;
+        public int HairIndex = 0;
 
         //The face index
-        public int FaceIndex;
+        public int FaceIndex = 0;
 
         //The nose index
-        public int NoseIndex;
+        public int NoseIndex = 0;
 
         //The shoe index
-        public int ShoesIndex;
+        public int ShoeIndex = 0;
 
         //The accessory index
-        public int AccessoryIndex;
-
-        //The dresser index
-        public int DresserIndex;
+        public int AccessoryIndex = -1;
 
         //Whether the player is bald
-        public bool IsBald;
+        public bool IsBald = false;
+
+        //The players hair color
+        public Color HairColor = new Color(193, 90, 50);
+
+        //The players eye color
+        public Color EyeColor = new Color(122, 68, 52);
     }
 }
