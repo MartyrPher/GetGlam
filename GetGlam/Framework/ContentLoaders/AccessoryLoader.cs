50f4a43a0dba2a0a4320c58446f811e9a7e56217
Chris <csgephardt@gmail.com>
2021-01-09 23:04:23 -0500

    Added AccessoryLoaders.cs and HairLoader.cs to refactor code.

---
+++ GetGlam/Framework/ContentLoaders/AccessoryLoader.cs
@@ -0,0 +1,89 @@
+﻿using GetGlam.Framework.DataModels;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using System.IO;
+
+namespace GetGlam.Framework.ContentLoaders
+{
+    public class AccessoryLoader
+    {
+        // Instance of ModEntry
+        private ModEntry Entry;
+
+        // Directory for Accessory files
+        private DirectoryInfo AccessoriesDirectory;
+
+        // Current Content Pack 
+        private IContentPack CurrentContentPack;
+
+        // Accessory Model
+        private AccessoryModel Accessory;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="modEntry">Instance of ModEntry</param>
+        /// <param name="contentPack">Current Content Pack</param>
+        public AccessoryLoader(ModEntry modEntry, IContentPack contentPack)
+        {
+            AccessoriesDirectory = new DirectoryInfo(Path.Combine(contentPack.DirectoryPath, "Accessories"));
+            Entry = modEntry;
+            CurrentContentPack = contentPack;
+        }
+        
+        /// <summary>
+        /// Loads a accessory from a Content Pack.
+        /// </summary>
+        public void LoadAccessory()
+        {
+            try
+            {
+                if (DoesAccessoryDirectoryExist())
+                {
+                    CreateAccessoryModel();
+                    SetAccessoryModelVariables();
+                }
+            }
+            catch
+            {
+                Entry.Monitor.Log($"{CurrentContentPack.Manifest.Name} accessories is empty. This pack was not added.", LogLevel.Warn);
+            }
+        }
+
+        /// <summary>
+        /// Gets the Accessory Model.
+        /// </summary>
+        /// <returns>The Accessory Model</returns>
+        public AccessoryModel GetAccessoryModel()
+        {
+            return Accessory;
+        }
+
+        /// <summary>
+        /// Checks if the Accessory Directory Exists.
+        /// </summary>
+        /// <returns>Whether the directory exists</returns>
+        private bool DoesAccessoryDirectoryExist()
+        {
+            return AccessoriesDirectory.Exists;
+        }
+
+        /// <summary>
+        /// Creates the accessory model by loading files.
+        /// </summary>
+        private void CreateAccessoryModel()
+        {
+            Accessory = CurrentContentPack.ReadJsonFile<AccessoryModel>("Accessories/accessories.json");
+            Accessory.Texture = CurrentContentPack.LoadAsset<Texture2D>("Accessories/accessories.png");
+        }
+
+        /// <summary>
+        /// Sets texture height and mod name in the model.
+        /// </summary>
+        private void SetAccessoryModelVariables()
+        {
+            Accessory.TextureHeight = Accessory.Texture.Height;
+            Accessory.ModName = CurrentContentPack.Manifest.Name;
+        }
+    }
+}
---
+++ GetGlam/Framework/ContentLoaders/HairLoader.cs
@@ -0,0 +1,89 @@
+﻿using GetGlam.Framework.DataModels;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using System.IO;
+
+namespace GetGlam.Framework.ContentLoaders
+{
+    public class HairLoader
+    {
+        // Instance of ModEntry
+        private ModEntry Entry;
+
+        // Directory where the hair files are stored
+        private DirectoryInfo HairDirectory;
+
+        // The model of the hair
+        private HairModel Hair;
+
+        // Current content pack being looked at
+        private IContentPack CurrentContentPack;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="modEntry">Instance of ModEntry</param>
+        /// <param name="contentPack">Current content pack</param>
+        public HairLoader(ModEntry modEntry, IContentPack contentPack)
+        {
+            HairDirectory = new DirectoryInfo(Path.Combine(contentPack.DirectoryPath, "Hairstyles"));
+            Entry = modEntry;
+            CurrentContentPack = contentPack;
+        }
+        
+        /// <summary>
+        /// Loads Hair from a Content Pack.
+        /// </summary>
+        public void LoadHair()
+        {
+            try
+            {
+                if (DoesHairDirectoryExists())
+                {
+                    CreateNewHairModel();
+                    SetHairModelVariables();
+                }
+            }
+            catch 
+            {
+                Entry.Monitor.Log($"{CurrentContentPack.Manifest.Name} hairstyles is empty. This pack was not added.", LogLevel.Warn);
+            }
+        }
+
+        /// <summary>
+        /// Get the current hair model.
+        /// </summary>
+        /// <returns>The hair model</returns>
+        public HairModel GetHairModel()
+        {
+            return Hair;
+        }
+
+        /// <summary>
+        /// Checks if the hair directory exists.
+        /// </summary>
+        /// <returns>If the hair directory exists</returns>
+        private bool DoesHairDirectoryExists()
+        {
+            return HairDirectory.Exists;
+        }
+
+        /// <summary>
+        /// Creates a new Hair Model.
+        /// </summary>
+        private void CreateNewHairModel()
+        {
+            Hair = CurrentContentPack.ReadJsonFile<HairModel>("Hairstyles/hairstyles.json");
+            Hair.Texture = CurrentContentPack.LoadAsset<Texture2D>("Hairstyles/hairstyles.png");
+        }
+
+        /// <summary>
+        /// Sets texture and mod name in the model.
+        /// </summary>
+        private void SetHairModelVariables()
+        {
+            Hair.TextureHeight = Hair.Texture.Height;
+            Hair.ModName = CurrentContentPack.Manifest.Name;
+        }
+    }
+}
