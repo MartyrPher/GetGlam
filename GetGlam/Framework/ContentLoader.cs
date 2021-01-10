82c1ef8b7779028d2ae6f1e2e9019179f328a019
Chris <csgephardt@gmail.com>
2021-01-09 23:02:21 -0500

    Added Comments to ContentLoader.cs.

--- GetGlam/Framework/ContentLoader.cs
+++ GetGlam/Framework/ContentLoader.cs
@@ -3,47 +3,56 @@ using System.IO;
 
 namespace GetGlam.Framework
 {
-    /// <summary>Class that loads Content for the game</summary>
+    /// <summary>
+    /// Class that loads Content for the game.
+    /// </summary>
     public class ContentLoader : IAssetLoader
     {
-        //Instance of ModEntry
+        // Instance of ModEntry
         ModEntry Entry;
 
-        //Instance of ContentPackHelper
+        // Instance of ContentPackHelper
         ContentPackHelper PackHelper;
 
-        /// <summary>ContentLoader's Constructor</summary>
+        /// <summary>
+        /// ContentLoader's Constructor.
+        /// </summary>
         /// <param name="entry">Instance of ModEntry</param>
         /// <param name="packHelper">Instance of ContentPackHelper</param>
         public ContentLoader(ModEntry entry, ContentPackHelper packHelper)
         {
-            //Set the fields
+            // Set the fields
             Entry = entry;
             PackHelper = packHelper;
         }
 
-        /// <summary>Whether SMAPI can load the asset</summary>
+        /// <summary>
+        /// Whether SMAPI can load the asset.
+        /// </summary>
         /// <typeparam name="T">The type of asset</typeparam>
         /// <param name="asset">The asset in question</param>
         /// <returns>Wether it can load the asset</returns>
         public bool CanLoad<T>(IAssetInfo asset)
         {
-            if (asset.AssetNameEquals($"Mods/{Entry.ModManifest.UniqueID}/dresser.png"))
-                return true;
-            if (asset.AssetName.StartsWith("GetGlam_"))
+            if (asset.AssetNameEquals($"Mods/{Entry.ModManifest.UniqueID}/dresser.png") || asset.AssetName.StartsWith("GetGlam_"))
                 return true;
 
             return false;
         }
 
-        /// <summary>Whether SMAPI can load a given asset</summary>
+        /// <summary>
+        /// Whether SMAPI can load a given asset.
+        /// </summary>
         /// <typeparam name="T">The assets type</typeparam>
         /// <param name="asset">The asset in question</param>
         /// <returns>The specific asset to load else default</returns>
         public T Load<T>(IAssetInfo asset)
         {
+            // If asset to edit is dresser, load the dresser image.
             if (asset.AssetNameEquals($"Mods/{Entry.ModManifest.UniqueID}/dresser.png"))
                 return Entry.Helper.Content.Load<T>(Path.Combine("assets", "dresser.png"));
+
+            // If asset is GetGlam then load the player base
             if (asset.AssetName.StartsWith("GetGlam_"))
                 return (T)(object)PackHelper.LoadPlayerBase();
 
